6a9538a53f5b8badd60d5ad29db11223d28990d7
leonmsindwana <lennymsindwana@gmail.com>
2025-05-06 13:48:47 +0200

    chap_7

---
+++ my code/chapter_06/chap_6.py
@@ -0,0 +1,16 @@
+myself = {
+    "eyes": "brown",
+    "hair": "black",
+    "car": ["2 series(legs)", "BMW", "AUDI"]
+}
+
+random_dictionary = {'color':'red','points': 7}
+print(random_dictionary['color'])
+print(random_dictionary['points'])
+
+
+new_key = random_dictionary.get('car', 'BMW')
+print(new_key)
+
+
+print(num(range(2, 20)))
---
+++ my code/chapter_07/chap_7.py
@@ -0,0 +1,53 @@
+# my_name = input("what is your name? ")
+# print(my_name)
+
+
+# f_name = input("what is your first name")
+
+# print(f"hello {f_name}")
+
+# f_name += input("what is your last name")
+
+# print(f"hello {f_name}")
+
+
+# age = int(input("how old are you"))
+
+# if age < 18:
+#     print("you are too young")
+# elif age > 18:
+#     print("have a beer") 
+
+
+# number = 0
+
+# while number < 10:
+#     print(number)
+#     number += 1
+
+# print("finished")
+
+# prompt = "\nTell me something, and I will repeat it back to you:"
+# prompt += "\nEnter 'quit' to end the program. "
+# message = ""
+
+# while message != 'quit':
+#     message = input(prompt)
+#     print(message)
+
+
+
+random_person = {   
+    "first_name": "leon",
+    "last_name": "msindwana",
+    "age" : "19",
+    "city": "Jozi"
+
+}
+
+print(random_person["first_name"])
+print(random_person["last_name"])
+print(random_person["age"])
+print(random_person['city'])
+
+
