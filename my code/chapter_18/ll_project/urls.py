1d93ebc299b1e9e1a954d6205c7ba47bccb31c35
leonmsindwana <lennymsindwana@gmail.com>
2025-05-20 15:07:01 +0200

    chap

--- my code/chapter_18/ll_project/urls.py
+++ my code/chapter_18/ll_project/urls.py
@@ -1,7 +1,8 @@
-"""ll_project URL Configuration
+"""
+URL configuration for ll_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
